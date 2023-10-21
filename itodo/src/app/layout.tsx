3a6ca21808acca889813e0ef380fa4d552ebe2df
esdras4757 <elara@ahorra.io>
2023-10-20 18:09:49 -0600

    cambio en el layout del lado del cliente

--- itodo/src/app/layout.tsx
+++ itodo/src/app/layout.tsx
@@ -1,4 +1,3 @@
-'use client';
 import './globals.css'
 import { Inter } from 'next/font/google'
 import 'bootstrap/dist/css/bootstrap.min.css';
@@ -11,6 +10,7 @@ import io from "socket.io-client";
 import { notification } from 'antd';
 import openNotification from "./utils/notify";
 import { useEffect } from 'react';
+import LayoutClient from './Components/LayoutClient';
 const socket = io("http://localhost:5500");
 const inter = Inter({ subsets: ['latin'] })
 
@@ -24,58 +24,15 @@ export default function RootLayout({
 }: {
   children: React.ReactNode
 }) {
-
   
 
-  const playNotifications=(data:{
-    title:string
-
-  })=>{
-    const audio = new Audio('/audio/consequence-544.mp3');
-    audio.loop = true;
-    audio.play();
-    openNotification("info", data.title,0,
-    <div className='row justify-between col-12 mt-3'>
-      <div 
-      onClick={e=>{
-        socket.emit('responseEvent', 'holaa');
-      }}
-      style={{color:'red', borderRight:'1px solid Black'}}
-      className='col-6 cursor-pointer text-center p-0'>
-        Posponer (5min)
-      </div>
-      <div 
-      onClick={e=>{
-        notification.destroy()
-        audio.pause();
-        audio.currentTime=0;
-      }}
-       style={{color:'blue'}}
-      className='col-6 text-center cursor-pointer p-0'>
-        Aceptar
-      </div>
-    </div>
-    );
-  }
-
-  useEffect(() => {
-    const alertHandler = (data:any) => {
-      playNotifications(data);
-    };
-  
-    socket.on("alert", alertHandler);
-  
-    return () => {
-      socket.off("alert", alertHandler);  // Aquí se elimina el oyente
-      socket.disconnect();
-    };
-  }, []);
 
   return (
     <html lang="en">
       <body className={inter.className}>
       <ConfigProvider locale={es}>
         <Providers>
+        <LayoutClient/>
         {children}
         </Providers>
         </ConfigProvider>
