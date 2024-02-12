c853a061a1ea13a675d52ff52ac55cd46dc6d20c
esdras4757 <esdras4757@gmail.com>
2024-02-12 02:04:54 -0600

    ia para tareas completa2

--- server/index.js
+++ server/index.js
@@ -18,7 +18,7 @@ const { Server } = require("socket.io");
 
 app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
 app.use(cors({
-    origin: ['http://localhost:3000', 'http://tu-front-end.com'], // Reemplaza con la URL de tu frontend
+    origin: ['http://localhost:3000', 'https://i-to-do-esdras4757.vercel.app'], // Reemplaza con la URL de tu frontend
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
     credentials: true,
 }));
