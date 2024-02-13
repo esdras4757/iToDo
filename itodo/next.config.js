841cc1ea6d970fac2722c4695deb916ded0bfadb
esdras4757 <esdras4757@gmail.com>
2024-02-12 18:33:13 -0600

    configurado para aws

--- itodo/next.config.js
+++ itodo/next.config.js
@@ -1,10 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   // output: 'export',
+  output: 'export',
+    distDir: 'dist',
     typescript: {
        ignoreBuildErrors: true,
     },
   };
+
   
 
 module.exports = nextConfig
